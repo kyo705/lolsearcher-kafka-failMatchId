fd359ab153d02b74f5cb5060923f6ac53e804df8
kyo705 <mission0705@naver.com>
2022-12-10 16:11:02 +0900

    [ADD] 기능 요구 사항을 수행할 Service 추가
    - FailureMatchIdService : 응답 실패한 matchId들을 다루는 서비스 클래스
    - SuccessMatchService : 응답 성공한 match들을 다루는 서비스 클래스

---
+++ src/main/java/com/lolsearcher/persistance/failmatchids/service/SuccessMatchService.java
@@ -0,0 +1,13 @@
+package com.lolsearcher.persistance.failmatchids.service;
+
+import com.lolsearcher.persistance.failmatchids.model.entity.match.Match;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuccessMatchService {
+    public void saveSuccessMatches(List<Match> successMatches){
+
+    }
+}
