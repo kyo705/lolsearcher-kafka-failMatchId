c0025de9c35594c7edffb5be48a1c95138a4da8a
kyo705 <mission0705@naver.com>
2022-12-27 01:42:24 +0900

    [FIX] sleep된 스레드가 리벨런싱되지 않도록 수정
    기존의 컨슈머 스레드가 REST API 요청 실패시 sleep이 된 상황에서 리벨런싱이 일어난다면 해당 컨슈머는 다시 파티션을 할당받고 poll을 하게된다. 즉, 기존 sleep된 스레드에 interrupt가 발생해 예외가 발생한다. 해당 버그를 수정하기 위해 스레드 sleep 동안 해당 컨슈머는 리밸런싱이 일어나지 않도록 listenerContainer의 pause와 resume을 이용해 해결하였음

--- src/main/java/com/lolsearcher/persistance/failmatchids/service/kafka/consumer/FailMatchIdConsumerService.java
+++ src/main/java/com/lolsearcher/persistance/failmatchids/service/kafka/consumer/FailMatchIdConsumerService.java
@@ -12,9 +12,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import static java.util.Objects.requireNonNull;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class FailMatchIdConsumerService {
 
     private final RiotGamesApiService riotGamesApiService;
     private final SuccessMatchProducerService successMatchProducerService;
+    private final KafkaListenerEndpointRegistry registry;
 
     /**
      * 해당 애플리케이션 시작점
@@ -46,16 +50,29 @@ public class FailMatchIdConsumerService {
             acknowledgment.acknowledge();
 
         }catch (WebClientResponseException e){
-            if(e.getStatusCode() != HttpStatus.TOO_MANY_REQUESTS){
-                throw e;
-            }
-            try {
-                Thread.sleep(2*60*1000);
-            } catch (InterruptedException ex) {
-                log.error(ex.getMessage());
-            }
+            handleWebClientResponseException(e);
         }catch (Exception e){
             log.error(e.getMessage());
+            requireNonNull(registry.getListenerContainer(FailMatchIdConsumerConstants.LISTENER_ID)).destroy();
+        }
+    }
+
+    private void handleWebClientResponseException(WebClientResponseException e) {
+
+        if(e.getStatusCode() != HttpStatus.TOO_MANY_REQUESTS){
+            log.error(e.getMessage());
+            requireNonNull(registry.getListenerContainer(FailMatchIdConsumerConstants.LISTENER_ID)).destroy();
+        }
+
+        try {
+            requireNonNull(registry.getListenerContainer(FailMatchIdConsumerConstants.LISTENER_ID)).pause();
+
+            Thread.sleep(RiotGamesConstants.REQUEST_WAIT_MS);
+
+            requireNonNull(registry.getListenerContainer(FailMatchIdConsumerConstants.LISTENER_ID)).resume();
+        } catch (InterruptedException ex) {
+            log.error(ex.getMessage());
+            requireNonNull(registry.getListenerContainer(FailMatchIdConsumerConstants.LISTENER_ID)).destroy();
         }
     }
 }
